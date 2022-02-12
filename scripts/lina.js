f89ce9492fccb0fa26c300eb7a44e08d9f4593d8
84267 <93382021+84267@users.noreply.github.com>
2022-02-12 14:33:42 +0100

    quiz

--- scripts/lina.js
+++ scripts/lina.js
@@ -3,21 +3,24 @@ function check(){
 	let question1 = document.quiz.question1.value;
 	let question2 = document.quiz.question2.value;
 	let question3 = document.quiz.question3.value;
+	let question4 = document.quiz.question4.value;
+	let question5 = document.quiz.question5.value;
+	let question6 = document.quiz.question6.value;
 	let correct = 0;
 
 
-	if (question1 == "Providence") {
+	if (question1 == "5 jaar") {
 		correct++;
 }
-	if (question2 == "Hartford") {
+	if (question4 == "Evie") {
 		correct++;
 }	
-	if (question3 == "Albany") {
+	if (question5 == "Vlucht 537") {
 		correct++;
 	}
 	
-	let pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-	let messages = ["Great job!", "That's just okay", "You really need to do better"];
+	let pictures = ["../media/win.gif", "../media/meh.gif", "../media/lose.gif"];
+	let messages = ["Goed gedaan je hebt alles goed!", "Helaas niet alles goed..", "Helaas, alles fout!"];
 	let score;
 
 	if (correct == 0) {
@@ -35,6 +38,6 @@ function check(){
 	document.getElementById("after_submit").style.visibility = "visible";
 
 	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("number_correct").innerHTML = "Je hebt " + correct + " goed.";
 	document.getElementById("picture").src = pictures[score];
 	}
