959bf28c5f02476148cd4a44397f8490e6c8a483
Vera87961 <87961@glr.nl>
2022-02-16 19:17:15 +0100

    efidjs

--- scripts/vera.js
+++ scripts/vera.js
@@ -26,6 +26,71 @@ let myQuestions = [
         },
         correctAnswer: 'B'
     },
+    {
+        question: "Hoelang duurde het voordat het Duitse leger Nederland had bezet?",
+        answers: {
+            A: '1 maand of langer </br>',
+            B: '2 weken </br>',
+            C: 'minder dan een week </br>'
+        },
+        correctAnswer: 'C'
+    },
+    {
+        question: "Hoe heette de aanval op Rusland?",
+        answers: {
+            A: 'Operatie Barbossa </br>',
+            B: 'Operatie Himmler </br>',
+            C: 'Operatie Mercurius </br>'
+        },
+        correctAnswer: 'A'
+    },
+    {
+        question: "Tussen welke twee landen begon de oorlog?",
+        answers: {
+            A: 'Duitsland - Tjeschië </br>',
+            B: 'Duitsland - Nederland </br>',
+            C: 'Duitsland - Polen</br>'
+        },
+        correctAnswer: 'C'
+    },
+    {
+        question: "Hoe is Adolf Hitler uiteindelijk doodgegaan?",
+        answers: {
+            A: 'Hij kon de gasrekening niet meer betalen </br>',
+            B: 'Hij en zijn vrouw hebben samen zelfmoord gepleegd in de bunker </br>',
+            C: 'De Amerikanen hebben hem neergeschoten </br>'
+        },
+        correctAnswer: 'B'
+    },
+    {
+        question: "Wanneer en wat was D-Day?",
+        answers: {
+            A: '1945, bevrijding west-Europa</br>',
+            B: '1944, begin van de bevrijding </br>',
+            C: '1942, de dag dat Hitler verloor </br>'
+        },
+        correctAnswer: 'B'
+    },
+    {
+        question: "Naar welk kamp werd je eerst gestuurd voordat je naar Auschwitz?",
+        answers: {
+            A: 'Doorvoerkamp Vught </br>',
+            B: 'Doorvoerkamp Bergen-Belsen </br>',
+            C: 'Doorvoerkamp Westerbork </br>'
+        },
+        correctAnswer: 'C'
+    },
+    {
+        question: "Hoeveel concentratiekampen waren er in de Tweede Wereldoorlog (heel de wereld)?",
+        answers: {
+            A: 'Meer dan 42.000 </br>',
+            B: '10  </br>',
+            C: '1000 </br>'
+        },
+        correctAnswer: 'A'
+    },
+
+
 ];
 
 let quizContainer = document.getElementById('quiz');
